aad288956f559b7ffb04ae662e36bb4db91b44dd
tempesttswinson <tempestt.swinson@gmail.com>
2020-06-22 15:25:51 -0400

    added CRUD operartion on User Entities to handle signup, adduser, showupdate, update user and delete user

---
+++ src/main/java/com/win/tutorial/Domain/User.java
@@ -0,0 +1,60 @@
+package com.win.tutorial.Domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    // makes field required
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+        // needed for JPA
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User [email=" + email + ", id=" + id + ", name=" + name + "]";
+    }
+
+}
